17e8d54f18b36096418916e7c384f31843918393
€%er Padilla! <eder.padilla97@gmial.com>
2022-07-18 19:36:02 -0500

    Initial Commit

---
+++ Weather-SwiftUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather-SwiftUi
+//
+//  Created by Eder  Padilla on 18/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather-SwiftUi/Weather_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUiApp.swift
+//  Weather-SwiftUi
+//
+//  Created by Eder  Padilla on 18/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
