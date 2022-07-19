f3a4905a2019e03084d14e0d7540b79885265a62
€%er Padilla! <eder.padilla97@gmial.com>
2022-07-18 20:35:01 -0500

    Extract subview

--- Weather-SwiftUi/ContentView.swift
+++ Weather-SwiftUi/ContentView.swift
@@ -20,8 +20,24 @@ struct ContentView: View {
                                   weight: .medium,
                                   design: .default))
                     .foregroundColor(.white)
-                    .padding(.bottom, 100)
-                    .padding(.leading, 100)
+                    .padding()
+                VStack(spacing: 8) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                        .clipped()
+                    
+                    Text("76°C")
+                        .font(.system(size: 70,
+                                      weight: .medium))
+                        .foregroundColor(.white)
+                }
+                
+                HStack {
+                    WeatherDayView()
+                }
                 Spacer()
             }
         }
@@ -31,5 +47,28 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    var body: some View {
+        VStack {
+            Text("Tue")
+                .font(.system(size: 16,
+                              weight: .medium))
+                .foregroundColor(.white)
+            
+            Image(systemName: "cloud.sun.fill")
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+                .clipped()
+            
+            Text("76°")
+                .font(.system(size: 28,
+                              weight: .medium))
+                .foregroundColor(.white)
+        }
     }
 }
