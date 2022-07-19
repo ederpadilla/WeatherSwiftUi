695715666b631e125a94dd68245a005eea5b3121
€%er Padilla! <eder.padilla97@gmial.com>
2022-07-18 19:45:36 -0500

    init the project

--- Weather-SwiftUi/ContentView.swift
+++ Weather-SwiftUi/ContentView.swift
@@ -9,8 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            HStack {
+                Label(/*@START_MENU_TOKEN@*/"Label"/*@END_MENU_TOKEN@*/, systemImage: /*@START_MENU_TOKEN@*/"42.circle"/*@END_MENU_TOKEN@*/)
+                VStack {
+                    Text("Hello, world!")
+                        .padding()
+                    Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                }
+            }
+            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
